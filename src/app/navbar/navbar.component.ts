115f9f738cb5fc0a3adcc6acd6e15869861eff9b
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-10 21:59:17 +0300

    fixed navbar toggle bug

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  name = 'Angular ' + VERSION.major;
+  navbarCollapsed = true;
 
+  toggleNavbarCollapsing() {
+    this.navbarCollapsed = !this.navbarCollapsed;
+  }
 }
