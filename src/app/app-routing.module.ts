c4528c9fc1710550ae38a316d052313e8daf4576
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-17 02:07:14 +0300

    add route for signupComponent and fixing link in loginForm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { QuizComponent } from './components/quiz/quiz.component';
 import { AddQuizComponent } from './components/add-quiz/add-quiz.component';
 import { AuthButtonComponent } from './components/navbar/auth-button/auth-button.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupButtonComponent } from './components/navbar/signup-button/signup-button.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'addQuiz', component: AddQuizComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'signup', component: SignupFormComponent}
 ];
 
 @NgModule({
