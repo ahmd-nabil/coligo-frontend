07627fb84a7f071241068c570d56967da87daebb
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-21 12:37:10 +0300

    add signupRequest model

---
+++ src/app/model/signup-request.model.ts
@@ -0,0 +1,6 @@
+export class SignupRequest {
+    constructor(public first_name: string,
+                public last_name: string,
+                public email: string,
+                public password: string) {}
+}
